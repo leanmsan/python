638f4514449ce1b35e9b33c2c31d037d913ad0e2
Leandro Sánchez <leanmsan@gmail.com>
2023-02-23 17:14:18 -0300

    Funciones Complejas
    Signed-off-by: Leandro Sánchez <leanmsan@gmail.com>

---
+++ primeros-pasos/funciones-con-parametros.py
@@ -0,0 +1,6 @@
+def nombre(nombre, puesto = "Desconocido"):
+    print(f"Hola, Soy {nombre}, y soy {puesto}")
+
+nombre("Leandro", "Programador")
+
+nombre("Juan")
---
+++ primeros-pasos/funciones-con-retorno.py
@@ -0,0 +1,17 @@
+def nombre(nombre):
+    return nombre
+
+empleado = nombre("Leandro")
+
+print(empleado)
+
+
+def num_mayor():
+    if 25 > 24 :
+        return True
+    else :
+        return False
+    
+num = num_mayor()
+
+print(num)
