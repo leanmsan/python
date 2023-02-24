530bd6cb5d7cef70ac2e6dd416ae4bd25b00b779
Leandro Sánchez <leanmsan@gmail.com>
2023-02-24 16:45:27 -0300

    Condiciones
    Signed-off-by: Leandro Sánchez <leanmsan@gmail.com>

---
+++ primeros-pasos/condiciones.py
@@ -0,0 +1,30 @@
+balance = 500
+
+if balance > 0:
+    print("Puedes Pagar")
+else :
+    print("No Puedes Pagar")
+
+likes = 201
+if likes >= 200:
+    print("Excelente, 200 likes")
+else : 
+    print("Casi llegas a los 200")
+
+lenguaje = "Python"
+
+if lenguaje == "Python":
+    print("Excelente eleccion")
+
+#Negando una condicion
+if not lenguaje == "R":
+    print("Mala eleccion")
+else :
+    print("Excelente eleccion")
+
+#boolean
+usuario_aut = True
+if usuario_aut:
+    print("Ingreso al sistema")
+else :
+    print("Acceso denegado")
