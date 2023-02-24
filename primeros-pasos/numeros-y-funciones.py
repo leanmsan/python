900631830bc79d6bb752343cbf8aec1c33c22cc9
Leandro Sánchez <leanmsan@gmail.com>
2023-02-24 16:04:53 -0300

    Numeros y Funciones
    Signed-off-by: Leandro Sánchez <leanmsan@gmail.com>

---
+++ primeros-pasos/numeros-y-funciones.py
@@ -0,0 +1,11 @@
+def suma(a = 0, b = 0):
+    print(a+ b)
+
+suma(2, 3)
+suma(10, 15)
+
+
+def resta(a, b):
+    print(a - b)
+
+resta(15, 10)
---
+++ primeros-pasos/numeros.py
@@ -0,0 +1,16 @@
+print(2)
+print(2.5)
+print(2+3)
+print(8-4)
+print(5*5)
+print(10/3)
+
+print(4**3)
+print(3**3)
+
+print((2+3)*4)
+
+numero = 20
+numero += 1
+print(numero)
+
