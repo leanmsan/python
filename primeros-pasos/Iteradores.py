2f0f3bce957f0d2d4e8a7ef802227ffb24ed7d71
Leandro Sánchez <leanmsan@gmail.com>
2023-02-24 16:30:34 -0300

    Iteradores
    Signed-off-by: Leandro Sánchez <leanmsan@gmail.com>

---
+++ primeros-pasos/Iteradores.py
@@ -0,0 +1,13 @@
+lenguajes = ["Python", "Java", "JavaScript", "R"]
+
+#Usando for para recorrer la lista
+for lenguaje in lenguajes:
+    print(f"Estoy aprendiendo {lenguaje}")
+
+#usando for
+for numero in range(0, 10):
+    print(numero)
+
+#incrementos en for
+for numero in range(0, 10, 2): #2 es el incremento
+    print(numero)    
